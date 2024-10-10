7e4360d6b4dc02d612e1ea17cce3aadd1a2e55f3
jack <redcatmiss@126.com>
2024-10-11 04:20:57 +0800

    feat(invoke): invoke boardcase listen reply

---
+++ invoke.go
@@ -0,0 +1,86 @@
+package go_redismq
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+func listenForResponse(ctx context.Context, req *InvoiceRequest, responseChan chan *InvoiceResponse) {
+	defer close(responseChan)
+
+	client := redis.NewClient(GetRedisConfig())
+	// Close Conn
+	defer func(client *redis.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Printf("MQStream Closs Redis Stream Client error:%s\n", err.Error())
+		}
+	}(client)
+	replyChannel := fmt.Sprintf("%s_%s:%s", req.Group, req.Method, req.MessageId)
+	pubSub := client.Subscribe(ctx, replyChannel)
+	defer func(pubSub *redis.PubSub) {
+		err := pubSub.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(pubSub)
+
+	ch := pubSub.Channel()
+	for msg := range ch {
+		var res *InvoiceResponse
+		err := json.Unmarshal([]byte(msg.Payload), &res)
+		if err != nil {
+			fmt.Printf("Error deserializing response: %v\n", err)
+			return
+		}
+
+		responseChan <- res
+		return
+	}
+}
+
+func Invoke(ctx context.Context, req *InvoiceRequest, timeoutSeconds int) *InvoiceResponse {
+	if timeoutSeconds <= 0 {
+		timeoutSeconds = 15
+	}
+	invokeId := fmt.Sprintf("%s%d", GenerateRandomAlphanumeric(6), CurrentTimeMillis())
+	req.MessageId = invokeId
+	// todo mark send reply faster than listen
+	send, err := Send(&Message{
+		Topic: "internal",
+		Tag:   "invoke",
+		Body:  MarshalToJsonString(req),
+	})
+	if err != nil {
+		return &InvoiceResponse{
+			Status:   false,
+			Response: fmt.Sprintf("Invoke error:%s", err.Error()),
+		}
+	} else if !send {
+		return &InvoiceResponse{
+			Status:   false,
+			Response: fmt.Sprintf("Invoke send failed"),
+		}
+	}
+	responseChan := make(chan *InvoiceResponse)
+	go listenForResponse(ctx, req, responseChan)
+	go func() {
+		time.Sleep(time.Duration(timeoutSeconds) * time.Second)
+		responseChan <- &InvoiceResponse{
+			Status:   false,
+			Response: "Timeout",
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		return &InvoiceResponse{
+			Status:   false,
+			Response: "Timeout",
+		}
+	case response := <-responseChan:
+		return response
+	}
+}
--- invoke_listener.go
+++ invoke_listener.go
@@ -4,12 +4,19 @@ import (
 	"context"
 	"fmt"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/redis/go-redis/v9"
 )
 
 type InvoiceRequest struct {
-	Group  string
-	Method string
-	Param  []byte
+	MessageId string `json:"messageId"`
+	Group     string `json:"group"`
+	Method    string `json:"method"`
+	Request   string `json:"request"`
+}
+
+type InvoiceResponse struct {
+	Status   bool   `json:"status"`
+	Response string `json:"response"`
 }
 
 type MessageInvokeListener struct {
@@ -24,31 +31,62 @@ func (t MessageInvokeListener) GetTag() string {
 }
 
 func (t MessageInvokeListener) Consume(ctx context.Context, message *Message) Action {
-	if message.CustomData != nil && fmt.Sprintf("%s", message.CustomData["Group"]) != Group {
-
+	var req *InvoiceRequest
+	err := UnmarshalFromJsonString(message.Body, &req)
+	if err != nil {
+		g.Log().Errorf(ctx, "MessageInvokeListener UnmarshalFromJsonString Body error %s", err.Error())
+		//ignore
+		return CommitMessage
+	}
+	if req == nil || req.Group != Group || len(req.MessageId) == 0 || len(req.Method) == 0 {
+		g.Log().Errorf(ctx, "MessageInvokeListener Invalid Request:%s", MarshalToJsonString(req))
+		//ignore
+		return CommitMessage
+	}
+	res := &InvoiceResponse{}
+	client := redis.NewClient(GetRedisConfig())
+	// Close Conn
+	defer func(client *redis.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Printf("MQStream Closs Redis Stream Client error:%s\n", err.Error())
+		}
+	}(client)
+	replyChannel := fmt.Sprintf("%s_%s:%s", req.Group, req.Method, req.MessageId)
+	if op, ok := invokeMap[req.Method]; ok {
+		// invoke method
+		response, err := op(ctx, req.Request)
+		if err != nil {
+			res.Response = fmt.Sprintf("%s", err.Error())
+			res.Status = false
+			client.Publish(ctx, replyChannel, MarshalToJsonString(res))
+		} else {
+			res.Response = response
+			res.Status = true
+			client.Publish(ctx, replyChannel, MarshalToJsonString(res))
+		}
 	} else {
-		g.Log().Debugf(ctx, "MessageInvokeListener ignore invoice methodName:%s")
+		res.Response = "error: method not found"
+		res.Status = false
+		client.Publish(ctx, replyChannel, MarshalToJsonString(res))
 	}
 	return CommitMessage
 }
 
 func init() {
-	RegisterListener(New())
+	RegisterListener(&MessageInvokeListener{})
 	fmt.Println("MessageInvokeListener RegisterListener")
 }
 
-func New() *MessageInvokeListener {
-	return &MessageInvokeListener{}
-}
+var invokeMap map[string]func(ctx context.Context, param string) (response string, err error)
 
-var invokeMap map[string]func(ctx context.Context, param []byte) (response []byte, err error)
-
-func RegisterInvoke(methodName string, op func(ctx context.Context, param []byte) (response []byte, err error)) {
+func RegisterInvoke(methodName string, op func(ctx context.Context, param string) (response string, err error)) {
 	if len(methodName) <= 0 || op == nil {
-		fmt.Printf("MQStream RegisterInvoke error methodName:%s or op:%p is nil", methodName, op)
+		fmt.Printf("MQStream RegisterInvoke error methodName:%s or op:%p is nil\n", methodName, op)
 	} else if _, ok := invokeMap[methodName]; ok {
 		fmt.Printf("MQStream RegisterInvoke error exist Old One:%s for op:%p\n", methodName, op)
 	} else {
 		invokeMap[methodName] = op
+		fmt.Printf("MQStream RegisterInvoke methodName:%s for op:%p\n", methodName, op)
 	}
 }
